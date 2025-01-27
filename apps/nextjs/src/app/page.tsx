8a0f8539fc0295442202d428d2215a428b23d806
Mike Fisher <mikefisher84@gmail.com>
2025-01-27 18:01:34 -0500

    update text

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-primary">T3</span> Turbo
+            Good Sam Vercel, Neon demo
           </h1>
           <AuthShowcase />
 
