9350a486e4f9c0ac2a739649d021c3d88026d6a3
Mike Fisher <mikefisher84@gmail.com>
2025-01-27 16:17:38 -0500

    revert post router

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -8,13 +8,24 @@ import { protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = {
   all: publicProcedure.query(({ ctx }) => {
-    return ctx.db.select().from(Post).orderBy(desc(Post.id)).limit(10);
+    // return ctx.db.select().from(schema.post).orderBy(desc(schema.post.id));
+    return ctx.db.query.Post.findMany({
+      orderBy: desc(Post.id),
+      limit: 10,
+    });
   }),
 
   byId: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
-      return ctx.db.select().from(Post).where(eq(Post.id, input.id));
+      // return ctx.db
+      //   .select()
+      //   .from(schema.post)
+      //   .where(eq(schema.post.id, input.id));
+
+      return ctx.db.query.Post.findFirst({
+        where: eq(Post.id, input.id),
+      });
     }),
 
   create: protectedProcedure
