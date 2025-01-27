7f8ce14234e0e26167779aa631a31247074a9439
Mike Fisher <mikefisher84@gmail.com>
2025-01-27 14:35:49 -0500

    auth and posts working

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,7 +1,10 @@
 import { relations, sql } from "drizzle-orm";
-import { pgTable, primaryKey } from "drizzle-orm/pg-core";
+import { primaryKey, pgTableCreator } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
+
+// Create the table creator with a custom naming strategy
+const pgTable = pgTableCreator((name) => `${name}`);
 
 export const Post = pgTable("post", (t) => ({
   id: t.uuid().notNull().primaryKey().defaultRandom(),
