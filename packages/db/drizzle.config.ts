7e4a94fe8334925d7291ee123d7ea0276d824304
Mike Fisher <mikefisher84@gmail.com>
2025-01-27 14:00:22 -0500

    drizzle

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,14 +1,10 @@
 import type { Config } from "drizzle-kit";
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error("Missing POSTGRES_URL");
-}
-
-const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
-
 export default {
-  schema: "./src/schema.ts",
   dialect: "postgresql",
-  dbCredentials: { url: nonPoolingUrl },
-  casing: "snake_case",
+  schema: "./src/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://default:L2puKFJZsxN6@ep-restless-queen-a4h7rxdn-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require"
+  },
 } satisfies Config;
